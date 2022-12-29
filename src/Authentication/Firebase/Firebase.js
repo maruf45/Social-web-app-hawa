0dccf721ce8bcbc009fa2fc888ea8dcf05d3f7f5
maruf45 <armaruf450@gmail.com>
2022-12-29 19:40:27 +0600

    added firebase and setup

---
+++ src/Authentication/Firebase/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQQB0JLFjj7XdI-hUEzAbP5TfpPQOSZts",
+  authDomain: "hawa-7d5f6.firebaseapp.com",
+  projectId: "hawa-7d5f6",
+  storageBucket: "hawa-7d5f6.appspot.com",
+  messagingSenderId: "231531604831",
+  appId: "1:231531604831:web:135a5bf0562169510424ea",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const Auth = getAuth(app);
+export default Auth;
--- src/Pages/Sign Up/SignUp.js
+++ src/Pages/Sign Up/SignUp.js
@@ -87,7 +87,10 @@ const SignUp = () => {
                             </div>
 
                             <div>
-                                <button type="submit" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-orange-500 border border-transparent rounded-md focus:outline-none hover:bg-orange-600 focus:bg-orange-700">
+                                <button type="submit" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-orange-500 border border-transparent rounded-md focus:outline-none hover:bg-orange-600 focus:bg-orange
+                                450+
+                                450
+                                -700">
                                     Create account
                                 </button>
                             </div>
